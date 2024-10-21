757f6cf2a5a3a820e427697cb6262ac3b8c34763
Shen <863461783@qq.com>
2024-10-21 16:34:20 +0800

    Initial Commit

---
+++ iChat-ios/iChat_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iChat_iosApp.swift
+//  iChat-ios
+//
+//  Created by 香饽饽zizizi on 2024/10/21.
+//
+
+import SwiftUI
+
+@main
+struct iChat_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
